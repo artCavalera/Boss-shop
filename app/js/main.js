123ddb617583d2d8e5f6dc0456e33d313b4f7207
artCavalera <tsykalartem@icloud.com>
2025-12-04 14:57:42 +0200

    added catalog and checkout page

--- app/js/main.js
+++ app/js/main.js
@@ -20,6 +20,53 @@ const swiperReviews = new Swiper('.reviews__slider', {
   },
   pagination: {
     el: '.reviews__pagination',
-    type: "fraction",
+    type: 'fraction',
   },
+})
+
+const rangeSlider = document.querySelector('.range__slider')
+const rangeMin = document.querySelector('.range__min')
+const rangeMax = document.querySelector('.range__max')
+
+noUiSlider.create(rangeSlider, {
+  start: [300, 3000],
+  step: 100,
+  range: {
+    min: 300,
+    max: 3000,
+  },
+  format: {
+    to: (value) => Math.round(value),
+    from: (value) => Number(value),
+  },
+})
+
+rangeSlider.noUiSlider.on('update', (values, handle) => {
+  if (handle === 0) {
+    rangeMin.value = values[0]
+  } else {
+    rangeMax.value = values[1]
+  }
+})
+
+rangeMin.addEventListener('change', () => {
+  rangeSlider.noUiSlider.set([rangeMin.value, null])
+})
+
+rangeMax.addEventListener('change', () => {
+  rangeSlider.noUiSlider.set([rangeMax.value, null])
+})
+
+const modeContainer = document.querySelector('.view-mode__container')
+const modeBtnGrid = document.querySelector('.view-mode__btn-grid')
+const modeBtnLine = document.querySelector('.view-mode__btn-line')
+
+modeBtnGrid.addEventListener('click', () => {
+  modeContainer.classList.add('view-mode__container--grid')
+  modeContainer.classList.remove('view-mode__container--line')
+})
+
+modeBtnLine.addEventListener('click', () => {
+  modeContainer.classList.add('view-mode__container--line')
+  modeContainer.classList.remove('view-mode__container--grid')
 })
