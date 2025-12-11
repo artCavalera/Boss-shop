de7d87d2aa1886a7cf3574debfb0411eb5268709
artCavalera <tsykalartem@icloud.com>
2025-12-11 15:55:12 +0200

    added adaptive blog

--- app/js/main.js
+++ app/js/main.js
@@ -1,69 +1,4 @@
-// const headerBtn = document.querySelector('.header__btn')
-// const menu = document.querySelector('.menu')
-
-// headerBtn.addEventListener('click', () => {
-//   menu.classList.toggle('menu--active')
-// })
-
-// const modeContainer = document.querySelector('.view-mode__container')
-// const modeBtnGrid = document.querySelector('.view-mode__btn-grid')
-// const modeBtnLine = document.querySelector('.view-mode__btn-line')
-
-// modeBtnGrid.addEventListener('click', () => {
-//   modeContainer.classList.add('view-mode__container--grid')
-//   modeContainer.classList.remove('view-mode__container--line')
-// })
-
-// modeBtnLine.addEventListener('click', () => {
-//   modeContainer.classList.add('view-mode__container--line')
-//   modeContainer.classList.remove('view-mode__container--grid')
-// })
-
-// const swiper = new Swiper('.look__slider', {
-//   loop: true,
-//   slidesPerView: 3,
-//   spaceBetween: 40,
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.look__slider-arrow-next',
-//     prevEl: '.look__slider-arrow-prev',
-//   },
-// })
-
-// const rangeSlider = document.querySelector('.range__slider')
-// const rangeMin = document.querySelector('.range__min')
-// const rangeMax = document.querySelector('.range__max')
-
-// noUiSlider.create(rangeSlider, {
-//   start: [300, 3000],
-//   step: 100,
-//   range: {
-//     min: 300,
-//     max: 3000,
-//   },
-//   format: {
-//     to: (value) => Math.round(value),
-//     from: (value) => Number(value),
-//   },
-// })
-
-// rangeSlider.noUiSlider.on('update', (values, handle) => {
-//   if (handle === 0) {
-//     rangeMin.value = values[0]
-//   } else {
-//     rangeMax.value = values[1]
-//   }
-// })
-
-// rangeMin.addEventListener('change', () => {
-//   rangeSlider.noUiSlider.set([rangeMin.value, null])
-// })
-
-// rangeMax.addEventListener('change', () => {
-//   rangeSlider.noUiSlider.set([rangeMax.value, null])
-// })
-
-const swiper = new Swiper('.reviews-swiper', {
+const swiperReviews = new Swiper('.reviews-swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -81,4 +16,77 @@ const swiper = new Swiper('.reviews-swiper', {
     nextEl: '.reviews-arrow-next',
     prevEl: '.reviews-arrow-prev',
   },
+
+  breakpoints: {
+    320: { slidesPerView: 2 },
+    480: { slidesPerView: 3 },
+    768: { slidesPerView: 4 },
+    1000: { slidesPerView: 6 },
+    1400: { slidesPerView: 8 },
+  },
+})
+
+const swiper = new Swiper('.look__slider', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 40,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.look__slider-arrow-next',
+    prevEl: '.look__slider-arrow-prev',
+  },
+})
+
+const headerBtn = document.querySelector('.header__btn')
+const menu = document.querySelector('.menu')
+
+headerBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu--active')
+})
+
+const modeContainer = document.querySelector('.view-mode__container')
+const modeBtnGrid = document.querySelector('.view-mode__btn-grid')
+const modeBtnLine = document.querySelector('.view-mode__btn-line')
+
+modeBtnGrid?.addEventListener('click', () => {
+  modeContainer.classList.add('view-mode__container--grid')
+  modeContainer.classList.remove('view-mode__container--line')
+})
+
+modeBtnLine?.addEventListener('click', () => {
+  modeContainer.classList.add('view-mode__container--line')
+  modeContainer.classList.remove('view-mode__container--grid')
+})
+
+const rangeSlider = document.querySelector('.range__slider')
+const rangeMin = document.querySelector('.range__min')
+const rangeMax = document.querySelector('.range__max')
+
+noUiSlider.create(rangeSlider, {
+  start: [300, 3000],
+  step: 100,
+  range: {
+    min: 300,
+    max: 3000,
+  },
+  format: {
+    to: (value) => Math.round(value),
+    from: (value) => Number(value),
+  },
+})
+
+rangeSlider.noUiSlider.on('update', (values, handle) => {
+  if (handle === 0) {
+    rangeMin.value = values[0]
+  } else {
+    rangeMax.value = values[1]
+  }
+})
+
+rangeMin.addEventListener('change', () => {
+  rangeSlider.noUiSlider.set([rangeMin.value, null])
+})
+
+rangeMax.addEventListener('change', () => {
+  rangeSlider.noUiSlider.set([rangeMax.value, null])
 })
